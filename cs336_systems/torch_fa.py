f7f2230ed5cc7ce46aa8cd8eb80e3e2be84dfdcb
felixdae <felixdae@gmail.com>
2025-07-27 20:40:44 +0800

    rename var

--- cs336_systems/torch_fa.py
+++ cs336_systems/torch_fa.py
@@ -15,24 +15,24 @@ def torch_fa_kernel(q, k, v, is_causal=False):
         q_tile = q[..., i * Bq:(i + 1) * Bq, :]
         o = torch.zeros_like(q_tile)
         denominator = torch.zeros(q_tile.shape[:-1], device=q_tile.device)
-        max_logit = torch.zeros(q_tile.shape[:-1], device=q_tile.device)
-        torch.fill_(max_logit, float('-inf'))
+        max_score = torch.zeros(q_tile.shape[:-1], device=q_tile.device)
+        torch.fill_(max_score, float('-inf'))
         for j in range(Tk):
             k_tile = k[..., j * Bk:(j + 1) * Bk, :]
             v_tile = v[..., j * Bk:(j + 1) * Bk, :]
             s = einsum(q_tile, k_tile, '... q d, ... k d -> ... q k') / math.sqrt(d)
             max_s = torch.max(s, dim=-1).values
-            new_max_logit = torch.max(max_logit, max_s)
-            p = torch.exp(s - new_max_logit[..., None])
-            adjustment = torch.exp(max_logit - new_max_logit)
+            new_max_score = torch.max(max_score, max_s)
+            p = torch.exp(s - new_max_score[..., None])
+            adjustment = torch.exp(max_score - new_max_score)
             denominator = torch.sum(p, dim=-1) + denominator * adjustment
 
             incr = einsum(p, v_tile, '... q k, ... k d -> ... q d')
             o = einsum(torch.diag_embed(adjustment), o, '... b b, ... b d -> ... b d') + incr
-            max_logit = new_max_logit
+            max_score = new_max_score
         o = o / denominator[..., None]
         output[..., i * Bq:(i + 1) * Bq, :] = o
-        logsumexp[..., i * Bq:(i + 1) * Bq] = max_logit + torch.log(denominator)
+        logsumexp[..., i * Bq:(i + 1) * Bq] = max_score + torch.log(denominator)
     return output, logsumexp
 
 
